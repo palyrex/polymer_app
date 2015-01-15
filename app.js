26aea75447738d7fc059731e2fddbc9eb41fa158
Pavel Redics <pavel.redics@gmail.com>
2015-01-15 16:29:21 +0000

    delete unnecessary stuff, put starter files in view

--- app.js
+++ app.js
@@ -1,75 +1,25 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
+// set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set folders to use in html
 app.use('/components',  express.static(__dirname + '/components'))
 app.use('/elements',  express.static(__dirname + '/elements'))
-app.use('/starter',  express.static(__dirname + '/starter'))
 app.use('/post-service',  express.static(__dirname + '/post-service'))
 app.use('/api',  express.static(__dirname + '/api'))
-
+app.use('/views',  express.static(__dirname + '/views'))
 app.use('/finished',  express.static(__dirname + '/finished'))
 
-// app.use('/', routes);
-// app.use('/users', users);
+// handle main page
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/starter/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-app.get('/done', function(req, res) {
-    res.sendFile(__dirname + '/finished/index.html');
-});
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-
 
 module.exports = app;
